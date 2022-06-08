a253c404a2a1d66f9f233f698bdfef41b296acb2
Netero <longvm2280>
2022-06-08 23:03:01 +0700

    Add new todo

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -37,11 +37,31 @@ function Todo() {
     putItems(newItems);
   };
 
+  const handleAdd = text => {
+    putItems([...items, {key: getKey(), text, done:false}]);
+  };
+
+  const [filter, setFilter] = React.useState('ALL');
+
+  const displayItems = items.filter(item => {
+    if (filter === 'ALL') return true;
+    if (filter === 'TODO') return !item.done;
+    if (filter === 'DONE') return item.done;
+  });
+  const handleFilterChange = value => setFilter(value);
+
   return (
     <div className="panel">
       <div className="panel-heading">
         ITSS ToDoアプリ
       </div>
+
+      <Input onAdd={handleAdd} />
+      <Filter
+        onChange={handleFilterChange}
+        value={filter}
+      />
+
       {items.map(item => (
         <TodoItem 
         key = {item.key} 
