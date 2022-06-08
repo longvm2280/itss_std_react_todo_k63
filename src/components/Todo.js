ea2f08f4dd561c29c780fa3ff79c311d0fdef823
Netero <longvm2280>
2022-06-08 23:07:04 +0700

    Save todo

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -19,13 +19,15 @@ import useStorage from '../hooks/storage';
 import {getKey} from "../lib/util";
 
 function Todo() {
-  const [items, putItems] = React.useState([
-      /* テストコード 開始 */
-    { key: getKey(), text: '日本語の宿題', done: false },
-    { key: getKey(), text: 'reactを勉強する', done: false },
-    { key: getKey(), text: '明日の準備をする', done: false },
-    /* テストコード 終了 */
-  ]);
+  // const [items, putItems] = React.useState([
+  //     /* テストコード 開始 */
+  //   { key: getKey(), text: '日本語の宿題', done: false },
+  //   { key: getKey(), text: 'reactを勉強する', done: false },
+  //   { key: getKey(), text: '明日の準備をする', done: false },
+  //   /* テストコード 終了 */
+  // ]);
+
+  const [items, putItems, clearItems] = useStorage();
 
   const handleCheck = checked => {
     const newItems = items.map(item => {
